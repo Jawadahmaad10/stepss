2335ce474c84555735ba13d9e5669bdf93e3f45a
Jawadahmaad10 <jawadjavedali@gmail.com>
2025-03-11 16:50:17 +0500

    steps

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
 export default function App(){
+
+  let step = 2;
+
+  function handlePrevious(){
+    
+  }
+
+  function handleNext(){
+    
+  }
+
+
   return <div className="steps">
     <div className="numbers">
-      <div>1</div>
-      <div>2</div>
-      <div>3</div>
+      <div className={`${step >=1  ? 'active' :  ""}` }>1</div>
+      <div className={`${step >=1  ? 'active' :  ""}` }>2</div>
+      <div className={`${step >=1  ? 'active' :  ""}` }>3</div>
     </div>
 
 
-    <p className="message">Hello</p>
+    <p className="message">
+      {step} : {messages[step-1]} 
+    </p>
 
     <div className="buttons">
-      <button>Previous</button>
-      <button>Next</button>
+      <button style={{backgroundColor: '#7950f2' , color: '#fff'}}
+      onClick={handlePrevious}>Previous</button>
+      <button style={{backgroundColor: '#7950f2' , color: '#fff'}}
+      onClick={handleNext}>Next</button>
+
 
     </div>
     </div>;
